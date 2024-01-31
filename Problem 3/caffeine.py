ea4ddbae436e4a39dd91fe4b89e2118d7563d675
rileyspollard <rileyspollard@outlook.com>
2024-01-31 14:46:38 -0500

    problem 3

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -2,6 +2,11 @@
 def caffeine():
     caffeine_mg = float(input())
     ''' Type your code here. '''
-    
+    caffeine_mg /= 2
+    print('After 6 hours: ' + f'{caffeine_mg:.2f}' + ' mg')
+    caffeine_mg /= 2
+    print('After 12 hours: ' + f'{caffeine_mg:.2f}' + ' mg')
+    caffeine_mg /= 4
+    print('After 24 hours: ' + f'{caffeine_mg:.2f}' + ' mg')
 if __name__ == "__main__":
     caffeine()
