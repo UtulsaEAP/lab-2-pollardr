81265d9c95fd234ac8b5c19b0c29d3786afa53d4
rileyspollard <rileyspollard@outlook.com>
2024-01-31 15:01:49 -0500

    all problems done

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -5,5 +5,11 @@ def simple_stats():
     num3 = float(input())
     num4 = float(input())
     ''' Type your code here. '''
+
+    result1 = num1*num2*num3*num4
+    avg = (num1+num2+num3+num4)/4
+    print(f'{result1:.0f}' + ' ' + f'{avg:.0f}')
+    print(f'{result1:.3f}' + ' ' + f'{avg:.3f}')
+
 if __name__ == "__main__":
     simple_stats()
