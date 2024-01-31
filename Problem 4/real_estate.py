7d12d665b6914522c328bf066389cb58f7f72baf
rileyspollard <rileyspollard@outlook.com>
2024-01-31 14:55:37 -0500

    problem 4

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -4,5 +4,12 @@ def real_estate():
     current_price = int(input())
     last_month_price = int(input())
     # Your code goes here
+
+    estmorg = (current_price * 0.051) / 12
+    change = current_price - last_month_price
+
+    print('This house is $' + str(current_price) + '. The change is $' + str(change) +  ' since last month.')
+    print('The estimated monthly mortgage is $' + f'{estmorg:.2f}' + '.')
+
 if __name__ == "__main__":
     real_estate()
