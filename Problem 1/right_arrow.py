bbe6ec11ff7734ab0f5a6c239b46ab4788f0c6c9
rileyspollard <rileyspollard@outlook.com>
2024-01-31 14:26:40 -0500

    problem 1

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -4,7 +4,10 @@ def right_arrow():
 
     row1 = '      ' + head_char
     ''' Type your code here. '''
-
+    row1 = '      ' + head_char
+    row2 = ''+ base_char + base_char + base_char +base_char + base_char + base_char + head_char + head_char
+    row3 = ''+ base_char + base_char + base_char +base_char + base_char + base_char + head_char + head_char + head_char
+    
     print(row1)
     print(row2)
     print(row3)
